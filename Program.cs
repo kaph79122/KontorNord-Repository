a658ca205f8d3581ec24751af66ff41f4cbaeae8
rasmus <rsla79145@edu.ucl.dk>
2026-02-27 21:06:01 +0100

    struktur

--- Program.cs
+++ Program.cs
@@ -13,13 +13,14 @@
                 Console.WriteLine("===============================================".PadLeft(50));
                 Console.WriteLine("                BOOK ET MØDELOKALE             ".PadLeft(50));
                 Console.WriteLine("===============================================".PadLeft(50));
-                
                 Console.WriteLine("");
                 Console.WriteLine("    tryk på en vilkårlist tast for at forsætte...");
                 Console.ReadKey();
 
 
                 Console.Clear();
+
+
                 Login(ref login);
 
                 // SKRIV DIT LOGIN HER NEEGA 
