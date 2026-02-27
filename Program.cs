b0fd21abce104fc074048642446d94f0e5562d58
rasmus <rsla79145@edu.ucl.dk>
2026-02-27 17:14:44 +0100

    s

--- Program.cs
+++ Program.cs
@@ -4,16 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Spiller du valorant? ja / nej?");
-            string svar = Console.ReadLine().ToLower();
-            if (svar == "nej")
-            {
-                Console.WriteLine("perfekt!");       
-            }
-            else if (svar =="ja")
-            {
-                Console.WriteLine("Oof");
-            }
+           
         
         
         }
