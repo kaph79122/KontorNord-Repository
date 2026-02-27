e54d8a83f1c962c9cf5d030256480eb60dfa6fb6
rasmus <rsla79145@edu.ucl.dk>
2026-02-27 17:13:42 +0100

    stavefejl

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
             }
             else if (svar =="ja")
             {
-                Console.WriteLine("Oof”");
+                Console.WriteLine("Oof");
             }
         
         
