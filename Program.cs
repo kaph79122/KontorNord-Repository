139371e3ca0fc0afb4ca4f6398cf2f8373b9012f
rasmus <rsla79145@edu.ucl.dk>
2026-02-27 13:24:16 +0100

    Indsat switch uden metoder

--- Program.cs
+++ Program.cs
@@ -5,23 +5,56 @@
         static void Main(string[] args)
         {
             bool ProgramKøre = true;
-            
-            
-            
-            while(ProgramKøre = true) 
+
+
+
+            while (ProgramKøre = true)
             {
-            
+
                 Console.WriteLine("===============================================");
                 Console.WriteLine("===================HOVEDMENU===================");
                 Console.WriteLine("===============================================");
 
-            
+                Console.WriteLine("1");
+                Console.WriteLine("2");
+                Console.WriteLine("3");
+                Console.WriteLine("4");
+                Console.WriteLine("5");
+
+                string brugerinput = Console.ReadLine().ToLower();
+
+                switch(brugerinput)
+                {
+                   case "1":
+                   break;
+                   
+                   case "2":
+                   break;
+
+                   case "3":
+                   break;
+                    
+                   case "4":
+                   break;
+                    
+                   case "5":
+                   break;
 
 
 
 
 
 
+
+                }
+
+
+
+
+
+
+
+            }
         }
     }
 }
